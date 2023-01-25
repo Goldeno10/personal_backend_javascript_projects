99696a47b862336e364b169424eb826ea359cd40
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-25 15:24:23 +0100

    ...

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let stringifyedEmployees = '';
-    let tracker = 0;
-    for (let i of reportWithIterator){
-        stringifyedEmployees += `${(tracker !== 0)? ' | ' : ''}${i}`;
-        tracker +=1 
-    }
-    return stringifyedEmployees
+  let stringifyedEmployees = '';
+  let tracker = 0;
+  for (const i of reportWithIterator) {
+    stringifyedEmployees += `${(tracker !== 0) ? ' | ' : ''}${i}`;
+    tracker += 1;
+  }
+  return stringifyedEmployees;
 }
