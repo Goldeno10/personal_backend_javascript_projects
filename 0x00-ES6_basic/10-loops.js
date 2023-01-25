ada8b850780dbbe9333f96f4e31612abe1b9a098
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-25 15:02:30 +0100

    ...

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const idx of array) {
-      array[array.indexOf(idx)] = appendString + idx;
-    }
-  
-    return array;
+  for (const idx of array) {
+    array[array.indexOf(idx)] = appendString + idx;
   }
+
+  return array;
+}
