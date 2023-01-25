71ceecda4ccd25f930d6f4aecc62c81bc2154151
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-25 15:21:51 +0100

    ...

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,4 @@
-export default function createIteratorObject(report){
+export default function createIteratorObject(report) {
   let arr = [];
   for (const obj in report.allEmployees) {
     if (obj) {
