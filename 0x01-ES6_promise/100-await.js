06fa547cab21eb9e443c3424561b600784fefb48
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-26 10:23:52 +0000

    ..

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
-    try {
-      const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-      console.log(photo)
-      return {
-        photo: photo,
-        user: user
-      };
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(photo);
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error(error);
   }
-  
+}
+
 asyncUploadUser();
