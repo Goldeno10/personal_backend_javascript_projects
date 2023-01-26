c79539636d2c9abb3c53d13800bab9b84cc92cda
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-26 10:00:23 +0000

    ..

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-promise.then(
-  {
-    'status': 200,
-    'body': 'Success'
-    })
-    .then(console.log('Got a response from the AP'))
-    .catch({'Error':''});
+  promise.then(
+    {
+      status: 200,
+      body: 'Success',
+    },
+  )
+    .finally(console.log('Got a response from the AP'))
+    .catch((err) => new Error(err));
 }
