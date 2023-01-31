a20831b01c7bbebfebf1b065ec40d6a443cd1be8
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-01-31 21:42:10 +0100

    initial comit

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,36 @@
+class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
