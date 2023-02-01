7197a255c6bf55dd2a327ad3f145374644a807e6
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-02-01 20:59:14 +0000

    Initial commit

---
+++ 0x04-TypeScript/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+  firstname: string;
+  lastname: string;
+  age: number;
+  location: string;
+} 
+
+const studentOne: Student = {
+  fistname = 'Muhammad';
+  lastname = 'Gold';
+  age = 25;
+  lacation = 'Nigeria';
+}
+const studentTwo: Student = {
+  firstname = 'Ibrahim';
+  lastname = 'Genius';
+  age = 23;
+  lacation = 'Kaduna';
+}
+const studentsList = [studentOne, studentTwo];
