45a67ad3bdbb4ee6081ad1f4caa29a2dec90773b
Goldeno10 <ibrahimmuhammad271@gmail.com>
2023-02-07 13:19:15 +0100

    ...

--- 0x00-python_variable_annotations/4-define_variables.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-'''
-contain definitions of annotated variables with the specified values
-'''
-
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = 'Holberton'
